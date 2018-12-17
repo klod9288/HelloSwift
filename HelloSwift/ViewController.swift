97b71e76df0eae63df810e316391e6ef10b7f59e
srongklod <klod9288@gmail.com>
2018-12-17 10:42:55 +0700

    Initial Commit

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by Srongklod on 17/12/2561 BE.
+//  Copyright © 2561 Srongklod_B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
